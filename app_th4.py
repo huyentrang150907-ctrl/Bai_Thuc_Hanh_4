66cc353eae9c86b71e7842e3f3951bbc74fea8fe
trang <huyentrang150907@gmail.com>
2026-06-05 14:14:18 +0700

    Update app_th4.py

--- app_th4.py
+++ app_th4.py
@@ -47,7 +47,7 @@ if uploaded_file is not None:
                 grouped = df.groupby(cluster_col)
                 
                 # Gọi mô hình Gemini
-                model = genai.GenerativeModel('gemini-1.5-flash')
+                model = genai.GenerativeModel('gemini-pro')
                 
                 # Duyệt qua từng nhóm để gửi cho AI tóm tắt
                 for cluster_id, group_data in grouped:
